ea53dd0029144996cd71465903a1ab045266254b
iyas bagus <bagusiyas15@gmail.com>
2024-05-17 10:04:52 +0700

    Belajar Next JS

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>
+        <nav>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/blog'}>Blog</Link>
+        </nav>
+      </header>
+    <Component {...pageProps} />;
+
+  </>)
 }
